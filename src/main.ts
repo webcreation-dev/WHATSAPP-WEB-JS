e395d43b8181763404589d7f0bd0a73b6392075f
webcreation-dev <adjilan2403@gmail.com>
2025-10-15 05:47:58 +0100

    add ui to scan qr code

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors();
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
 
   const port = process.env.PORT || 3323;
   await app.listen(port);
